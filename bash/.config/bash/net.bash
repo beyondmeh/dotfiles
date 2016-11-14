c89ab1a4657e8e8f761b83e2fa090fed7c06e917
beyondmeh <224660+beyondmeh@users.noreply.github.com>
2016-11-14 13:01:15 -0500

    now in the linux-commands repo

--- bash/.config/bash/net.bash
+++ bash/.config/bash/net.bash
@@ -14,15 +14,3 @@ alias opennet='sudo lsof -i'
 # Websites
 alias google="firefox -P Browsing http://www.google.com"
 alias wiki="firefox -P Browsing http://en.wikipedia.org/wiki/Main_Page"
-
-# Get public IP address
-myip () { 
-    wget -q -O - checkip.dyndns.org | \
-    sed -e 's/.*Current IP Address: //' -e 's/<.*$//'; 
-}
-
-mylocalip() {
-    ip -o addr show wlp3s0 | \
-    head -n 1 | \
-    sed 's/.*inet \(\S*\)\/.*/\1/g'
-}
