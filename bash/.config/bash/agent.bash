04ed1460428471f0e57467d12e8d1e6ab717ccfc
beyondmeh <224660+beyondmeh@users.noreply.github.com>
2016-12-01 21:40:03 -0500

    using gpg-agent for ssh now

--- bash/.config/bash/agent.bash
+++ bash/.config/bash/agent.bash
@@ -3,25 +3,36 @@
 ##
 
 
-# SSH agent
-SSH_ENV="$HOME/.cache/ssh-env"
+## SSH agent
+#SSH_ENV="$HOME/.cache/ssh-env"
 
-function start_agent {
-     ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-     chmod 600 "${SSH_ENV}"
-     . "${SSH_ENV}" > /dev/null
-}
-
-if [ -f "${SSH_ENV}" ]; then
-     . "${SSH_ENV}" > /dev/null
-     if ! pgrep -u $USER ssh-agent > /dev/null; then
-         start_agent
-     fi
-else
-    start_agent
-fi
+#function start_agent {
+#     ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+#     chmod 600 "${SSH_ENV}"
+#     . "${SSH_ENV}" > /dev/null
+#}
+#
+#if [ -f "${SSH_ENV}" ]; then
+#     . "${SSH_ENV}" > /dev/null
+#     if ! pgrep -u $USER ssh-agent > /dev/null; then
+#         start_agent
+#     fi
+#else
+#    start_agent
+#fi
 
 # GPG agent
-echo BYE | gpg-connect-agent &> /dev/null
-GPG_TTY=$(tty)
-export GPG_TTY
+if ! pgrep -x -u "${USER}" gpg-agent >/dev/null 2>&1; then
+    gpg-connect-agent /bye >/dev/null 2>&1
+fi
+
+export GPG_TTY=$(tty)
+
+# Refresh gpg-agent tty in case user switches into an X session
+gpg-connect-agent updatestartuptty /bye >/dev/null
+
+# Set SSH to use gpg-agent
+unset SSH_AGENT_PID
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+    export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+fi
