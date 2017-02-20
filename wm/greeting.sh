bb93f7fce504001096d11b780f9a942c2ba9e56c
beyondmeh <224660+beyondmeh@users.noreply.github.com>
2017-02-19 20:08:25 -0500

    refactored

--- wm/greeting.sh
+++ wm/greeting.sh
@@ -14,19 +14,16 @@ TIME="$(date '+%l:%M %p')"
 HOUR=$(date '+%H')
 
 
-#
 # Sets the volume to an appropriete level given the time
-#
 if [ "$HOUR" -ge 8 ] && [ "$HOUR" -lt 21 ]; then
-    amixer -D pulse set Master 50%
+    PERC=50
 else
-    amixer -D pulse set Master 20%
+    PERC=20
 fi
+amixer -D pulse set Master ${PERC}% > /dev/null
 
 
-#
 # Plays a greeting depending on the time
-#
 if [ "$HOUR" -ge 17 ] || [ "$HOUR" -lt 5 ]; then
     GREETING="Good evening"
 elif [ "$HOUR" -ge 12 ]; then
@@ -36,4 +33,4 @@ else
 fi
 
 paplay "$HOME/bin/assets/sounds/startup.ogg"
-say.sh "$GREETING. Today is $DATE. It is $TIME"
+~/bin/say.sh "$GREETING. Today is $DATE. It is $TIME"
