4d880f82b743f6d6a16716b520a51eeaccbe6b32
beyondmeh <224660+beyondmeh@users.noreply.github.com>
2017-04-08 21:00:06 -0400

    removed unused section

--- bash/.config/bash/agent.bash
+++ bash/.config/bash/agent.bash
@@ -2,25 +2,6 @@
 ## SSH & GPG Agents
 ##
 
-
-## SSH agent
-#SSH_ENV="$HOME/.cache/ssh-env"
-
-#function start_agent {
-#     ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-#     chmod 600 "${SSH_ENV}"
-#     . "${SSH_ENV}" > /dev/null
-#}
-#
-#if [ -f "${SSH_ENV}" ]; then
-#     . "${SSH_ENV}" > /dev/null
-#     if ! pgrep -u $USER ssh-agent > /dev/null; then
-#         start_agent
-#     fi
-#else
-#    start_agent
-#fi
-
 # GPG agent
 if ! pgrep -x -u "${USER}" gpg-agent >/dev/null 2>&1; then
     gpg-connect-agent /bye >/dev/null 2>&1
