37b307debeb97d02e89c0a6356442302d0114d54
beyondmeh <224660+beyondmeh@users.noreply.github.com>
2017-04-11 20:37:54 -0400

    run agent only on home computer

--- bash/.config/bash/agent.bash
+++ bash/.config/bash/agent.bash
@@ -2,18 +2,23 @@
 ## SSH & GPG Agents
 ##
 
-# GPG agent
-if ! pgrep -x -u "${USER}" gpg-agent >/dev/null 2>&1; then
-    gpg-connect-agent /bye >/dev/null 2>&1
-fi
 
-export GPG_TTY=$(tty)
+# Run GPG agent only on home computer
+if [ $(hostname) == 'toshy' ]; then
 
-# Refresh gpg-agent tty in case user switches into an X session
-gpg-connect-agent updatestartuptty /bye >/dev/null
+	if ! pgrep -x -u "${USER}" gpg-agent >/dev/null 2>&1; then
+		gpg-connect-agent /bye >/dev/null 2>&1
+	fi
 
-# Set SSH to use gpg-agent
-unset SSH_AGENT_PID
-if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-    export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+	export GPG_TTY=$(tty)
+
+	# Refresh gpg-agent tty in case user switches into an X session
+	gpg-connect-agent updatestartuptty /bye >/dev/null
+
+	# Set SSH to use gpg-agent
+	unset SSH_AGENT_PID
+	if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+		export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+	fi
+
 fi
