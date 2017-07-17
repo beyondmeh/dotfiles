9b579269643061e3418666c2771364a1a9ddca9d
beyondmeh <224660+beyondmeh@users.noreply.github.com>
2017-07-17 19:22:38 -0400

    fixed permissions

--- server-etc/iptables/etc/firewall/block-countries.sh
+++ server-etc/iptables/etc/firewall/block-countries.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
-# Slightly modified from:
+# Adapted from Matt Wilcox's "Unexpected DDOS: Blocking China with ipset and iptables":
 # https://mattwilcox.net/web-development/unexpected-ddos-blocking-china-with-ipset-and-iptables
+#
+# If you're confused, here's a good guide for Bash associative array witchcraft:
+# http://www.artificialworlds.net/blog/2012/10/17/bash-associative-array-examples/
 
-# Create the ipset list
-ipset -N china hash:net
-ipset -N russia hash:net
 
-# remove any old list that might exist from previous runs of this script
-rm cn.zone
-rm ru.zone
+# Known bad actor states (from fail2ban logs, not personal opinion):
+declare -A COUNTRIES=( [cn]=china [in]=india [lv]=latvia [ru]=russia [ua]=ukraine [vn]=vietnam )
 
-# Pull the latest IP set for China & Russia
-wget -P . http://www.ipdeny.com/ipblocks/data/countries/cn.zone
-wget -P . http://www.ipdeny.com/ipblocks/data/countries/ru.zone
 
-# Add each IP address from the downloaded list into the ipset 'china'
-for i in $(cat /etc/cn.zone ); do ipset -A china $i; done
-for i in $(cat /etc/ru.zone ); do ipset -A russia $i; done
+for country in "${COUNTRIES[@]}"; do 
+	# Create the ipset list
+	ipset -N "$country" hash:net
+done
+
+for tld in "${!COUNTRIES[@]}"; do 
+	# remove old lists that might exist from previous runs
+	rm "${tld}.zone"
+	
+	# Pull the latest IP set for the country
+	wget -P . "http://www.ipdeny.com/ipblocks/data/countries/${tld}.zone" -o "${tld}.zone"
+done
+
+
+for tld in "${!COUNTRIES[@]}"; do 
+	# Make everything a little more readable
+	country=${COUNTRIES[$tld]}
+
+	# Add each IP address from the downloaded list into the ipset country
+	for i in $(cat "${tld}.zone" ); do 
+		ipset -A "$country" $i; 
+	done
+done
+
 
 # Restore iptables
 /sbin/iptables-restore < /etc/firewall/iptables.rules
