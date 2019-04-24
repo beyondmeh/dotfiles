d0d2814c96b4a93ffb1d1b111af9e2505b75292b
beyondmeh <224660+beyondmeh@users.noreply.github.com>
2019-04-24 14:57:09 -0400

    linux / bsd crossplatform support

--- .config/zsh/dotfiles-autoupdate.zsh
+++ .config/zsh/dotfiles-autoupdate.zsh
@@ -1,8 +1,15 @@
 UPDATE_AFTER=86400 # 1 day in seconds
 TIME=$(date +%s)
 
-# normal git repo
-LAST_FETCH=$(stat -f %m "$HOME/.git/FETCH_HEAD")
+# find last modification time for a normal git repo,
+# which should match it's last fetch
+if uname | grep -q FreeBSD; then
+	# BSD
+	LAST_FETCH=$(stat -f %m "$HOME/.git/FETCH_HEAD")
+else
+	# GNU coreutils
+	LAST_FETCH=$(stat -c %Y "$HOME/.git/FETCH_HEAD")
+fi
 
 if ((TIME - LAST_FETCH >= UPDATE_AFTER)); then
   # run in a subshell to avoid changing our current working dir
