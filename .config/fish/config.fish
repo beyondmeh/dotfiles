2f348b3712782301a7052967788963e3e35b64b1
beyondmeh <224660+beyondmeh@users.noreply.github.com>
2024-08-06 09:49:40 -0400

    helix editor support

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,97 +1,121 @@
-
 set -x -g LC_ALL en_US.UTF-8
 set -x -g LANG en_US.UTF-8
 
 if status is-interactive
-  # Commands to run in interactive sessions can go here
+    # Commands to run in interactive sessions can go here
 
-  # Set env variables
-  set -a PATH ~/bin
-  set -a PATH ~/.local/bin
-  set -a PATH /opt/homebrew/bin
-  set -a PATH /usr/local/bin  
-  set -a PATH ~/Library/Python/3.9/bin
+    fish_config theme choose "Dracula Official"
 
-  set -x EDITOR vim
+    # Set env variables
+    set -a PATH ~/bin
+    set -a PATH ~/.local/bin
+    set -a PATH /opt/homebrew/bin
+    set -a PATH /usr/local/bin
+    set -a PATH ~/Library/Python/3.9/bin
 
-  set -x PAGER less
-  if type -q most
-    set -x PAGER most
-  end
-
-  # functions
-  function ..    ; cd .. ; end
-  function ...   ; cd ../.. ; end
-  function ....  ; cd ../../..; end
-  function ..... ; cd ../../../.. ; end
-
-  function grep ; command grep --color=auto $argv ; end
-
-  function !!
-    if type -q sudo
-      eval sudo $history[1]
-    else if type -q doas
-      eval doas $history[1]
+    # Set perferred editors and pager 
+    if type -q hx
+        set -x EDITOR hx
+    else if type -q micro
+        set -x EDITOR micro
     else
-      su -c $history[1] root
+        set -x EDITOR vim
     end
-  end
 
-  function md --wraps mkdir -d "Create a directory and cd into it"
-    command mkdir -p $argv
-    if test $status = 0
-      switch $argv[(count $argv)]
-        case '-*'
-        case '*'
-          cd $argv[(count $argv)]
-          return
-      end
+    if type -q most
+        set -x PAGER most
+    else
+        set -x PAGER less
     end
-  end
 
-  if type -q exa
-    alias ls "exa"
-  else if type -q exz
-    alias ls "eza"
-  end
+    # Dracula theme support for micro editor
+    if type -q micro
+        set -x MICRO_TRUECOLOR 1
+    end
 
-  # Prefered programs
-  alias nano "micro"
-  alias dd "dc3dd"
-  alias du "ncdu" 
-  alias ping "gping"
-  alias cat "bat"
-  alias top "btop"
-  alias diff "colordiff"
-  alias wget "curl -O"
-  alias vi $EDITOR
-  alias vim $EDITOR
+    # functions
+    function ..
+        cd ..
+    end
+    function ...
+        cd ../..
+    end
+    function ....
+        cd ../../..
+    end
+    function .....
+        cd ../../../..
+    end
 
-  # new aliases
-  alias s "sudo"
-  alias sl "ls"
-  alias rd "rmdir"
-  alias mv "mv -i"
-  alias cp "cp -i"
+    function grep
+        command grep --color=auto $argv
+    end
 
-  alias ipv4 "dig -4 +short myip.opendns.com @resolver1.opendns.com"  
-  alias ipv6 "dig -6 +short myip.opendns.com @resolver1.opendns.com"
-  alias ip "ipv4 && ipv6"
-  alias dig "dig +nocmd any +multiline +noall +answer"
+    function !!
+        if type -q sudo
+            eval sudo $history[1]
+        else if type -q doas
+            eval doas $history[1]
+        else
+            su -c $history[1] root
+        end
+    end
 
-  alias rmdstore "find . -name '*.DS_Store' -type f -ls -delete"
+    function md --wraps mkdir -d "Create a directory and cd into it"
+        command mkdir -p $argv
+        if test $status = 0
+            switch $argv[(count $argv)]
+                case '-*'
+                case '*'
+                    cd $argv[(count $argv)]
+                    return
+            end
+        end
+    end
 
-  # print motd-like
-  #figlet -f mini $(hostname)
- 
-  if type -q fortune
-    tput setaf 5
-    fortune -s
-    tput sgr0
-  end      
-  
+    if type -q exa
+        alias ls exa
+    else if type -q exz
+        alias ls eza
+    end
+
+    # Prefered programs
+    alias nano micro
+    alias dd dc3dd
+    alias du ncdu
+    alias ping gping
+    alias cat bat
+    alias top btop
+    alias diff colordiff
+    alias wget "curl -O"
+    alias vi $EDITOR
+    alias vim $EDITOR
+
+    # new aliases
+    alias s sudo
+    alias sl ls
+    alias rd rmdir
+    alias mv "mv -i"
+    alias cp "cp -i"
+
+    alias ipv4 "dig -4 +short myip.opendns.com @resolver1.opendns.com"
+    alias ipv6 "dig -6 +short myip.opendns.com @resolver1.opendns.com"
+    alias ip "ipv4 && ipv6"
+    alias dig "dig +nocmd any +multiline +noall +answer"
+
+    alias rmdstore "find . -name '*.DS_Store' -type f -ls -delete"
+
+    # print motd-like
+    #figlet -f mini $(hostname)
+
+    if type -q fortune
+        tput setaf 5
+        fortune -s
+        tput sgr0
+    end
+
 end
 
-test fzf ; and fzf --fish | source
+test fzf; and fzf --fish | source
 
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
